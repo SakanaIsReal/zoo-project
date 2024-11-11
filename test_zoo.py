3a3b12c27542626cff2cd9e5fe81f7237569c7ba
SakanaIsReal <skyza.kchongklahan@gmail.com>
2024-11-11 12:25:30 +0700

    fix: correct the age calculating algorithm

--- test_zoo.py
+++ test_zoo.py
@@ -5,8 +5,47 @@ class TestZoo(unittest.TestCase):
     def setUp(self):
         self.zoo = Zoo()
 
-    def test_child_ticket_price(self):
-        self.assertEqual(self.zoo.get_ticket_price(5), 50)
+    def test_child_ticket_price_01(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), "Error: Age can't be negative number.")
+
+    def test_child_ticket_price_02(self):
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
+    
+    def test_child_ticket_price_03(self):
+        self.assertEqual(self.zoo.get_ticket_price(1), 50)
+    
+    def test_child_ticket_price_04(self):
+        self.assertEqual(self.zoo.get_ticket_price(11), 50)
+    
+    def test_child_ticket_price_05(self):
+        self.assertEqual(self.zoo.get_ticket_price(12), 50)
+           
+    def test_child_ticket_price_06(self):
+        self.assertEqual(self.zoo.get_ticket_price(13), 100)
+           
+    def test_child_ticket_price_07(self):
+        self.assertEqual(self.zoo.get_ticket_price(14), 100)
+           
+    def test_child_ticket_price_08(self):
+        self.assertEqual(self.zoo.get_ticket_price(19), 100)
+           
+    def test_child_ticket_price_09(self):
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+           
+    def test_child_ticket_price_10(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+           
+    def test_child_ticket_price_11(self):
+        self.assertEqual(self.zoo.get_ticket_price(22), 150)
+           
+    def test_child_ticket_price_12(self):
+        self.assertEqual(self.zoo.get_ticket_price(59), 150)
+           
+    def test_child_ticket_price_13(self):
+        self.assertEqual(self.zoo.get_ticket_price(60), 150)
+           
+    def test_child_ticket_price_14(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
        
     # Add your additional test cases here.
 
--- zoo.py
+++ zoo.py
@@ -1,10 +1,12 @@
 class Zoo:
     def get_ticket_price(self, age):
-        if 0 < age <= 12:
+        if age < 0:
+            return "Error: Age can't be negative number."
+        if 0 <= age <= 12:       # -1, 0, 1    | 11, 12 ,13
             return 50
-        elif 13 <= age < 20:
+        elif 13 <= age <= 20:    # 12, 13, 14  | 19, 20, 21
             return 100
-        elif 21 < age <= 60:
+        elif 21 <= age <= 60:    # 20, 21, 22  | 59, 60, 61
             return 150
-        elif age >= 60:
+        elif age > 60:         # 59, 60, 61
             return 100
